a8e0d867dfdcb19cf8eb666e9958e8f480f4dca2
Joshua Snider <jsnider@mtu.edu>
2022-12-05 23:29:01 -0500

    Fix IdleRPG char name

--- bot.py
+++ bot.py
@@ -339,7 +339,7 @@ class IRCConn(object):
       self.identify(os.getenv('GRP_PASS'))
     for chan in self.join_first:
       self.join(chan)
-    self.say('login spacecake ' + os.getenv('GRP_PASS'), 'idlerpg')
+    self.say('login SpaceyCakes ' + os.getenv('GRP_PASS'), 'idlerpg')
 
 class Bot(object):
   """

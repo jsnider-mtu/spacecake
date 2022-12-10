7ee2f73383960e891ab658c1753ffe9888d15126
Joshua Snider <jsnider@mtu.edu>
2022-12-10 13:10:08 -0500

    Debug line

--- com.py
+++ com.py
@@ -47,6 +47,7 @@ donger = ['ヽ〳 ՞ ᗜ ՞ 〵ง',
 
 def PMFuncs(cmd, args, data, conn):
   chan = data['nick']
+  print(cmd, chan)
   if cmd.lower() == 'openai_register':
     print('openai_register called')
     if len(args) != 0:
